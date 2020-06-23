ea29ac131b7056aa7228261cdfe25ffd5f8fc4b0
Ivan Paniagua <ivan.paniagua@gmail.com>
2020-06-22 21:11:51 -0400

    Update js-xlx-validation-formula.js

--- js-xlx-validation-formula.js
+++ js-xlx-validation-formula.js
@@ -27,3 +27,27 @@ function write_ws_xml_datavalidationFormula(validations) {
     o += '</x14:dataValidations></ext></extLst>';
     return o;
 }
+
+//You need to have the library Xlsx.js and modify the method write_ws_xml
+function write_ws_xml(idx, opts, wb, rels) {
+  // ...
+  if(ws['!merges'] != null && ws['!merges'].length > 0) o[o.length] = (write_ws_xml_merges(ws['!merges']));
+  if(ws['!dataValidation']) o[o.length] = write_ws_xml_datavalidation(ws['!dataValidation']);
+  // ...
+}
+
+//example Sheet1 with another spreadsheet e.g. People and the values to validate in the column F
+var wb = {
+  Sheets: {
+    Sheet1: {
+      '$ref': 'A1:Z99',
+      '!dataValidation': [
+        {sqref: 'A1:A99', formula : 'People!F:F'},
+      ]
+    }
+  },
+  SheetNames: ['Sheet1']
+}
+
+// Important! at the moment of export the Excel File, add the parameter ignoerEC to false. This is the last step.
+XLSX.writeFile(wb, "ExportForecast.xlsx", { ignoreEC : false });
